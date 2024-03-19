8add6b9adb8b57283b74700c2e66b584cfbba56f
jpadrono <joaopsmbraga@gmail.com>
2024-03-19 15:58:39 -0300

    esboço inicial

---
+++ sistemaDeMatriculas.c
@@ -0,0 +1,629 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define p printf
+#define s scanf
+#define periodo_ATUAL 2024.1]
+
+//Declaração das structs para lsitas encadeadas
+
+typedef struct _Aluno{
+  int codigo;
+  char nome[100], cpf[13];
+  //ponteiros auxiliares para listagem encadeada dos dados
+  struct _Aluno *prox;
+  /****************************************************************************
+   *  O ponteiro *ant será usado apenas para debugagem do código              *
+   * caso, ao fazer uma pesquisa na lista, o ptr *ant não começar apontado    *
+   * para NULL, significa que eu não comecei a busca no primeiro elemento     *
+   * e portanto devemos parar o código                                        *
+   ****************************************************************************/
+  struct _Aluno *ant;
+} Aluno;
+
+//
+Aluno *ptr_i_aluno = NULL;
+
+typedef struct _Disciplina{
+  int codigo, creditos;
+  char nome[100], professor[100];
+  struct _Disciplina *prox;
+  struct _Disciplina *ant;
+} Disciplina;
+
+Disciplina *ptr_i_disciplina = NULL;
+
+typedef struct _Matricula{
+  int aluno, disciplina;
+  float periodo;
+  struct _Matricula *prox;
+  struct _Matricula *ant;
+} Matricula;
+
+Matricula *ptr_i_matricula = NULL;
+
+//Declaração de variáveis
+/******************************************************************************
+ *  Para códigos grandes, uma boa sequência de ações é começar declarando as  *
+ * principais funções que serão necessárias para o código, em seguida,        *
+ * estruturar a lógica do código na main e por fim, escrever a lógica das     *
+ * funções no finla do código.                                                *
+ ******************************************************************************/
+
+void criar_matricula(Matricula **ptr);
+void criar_aluno(Aluno **ptr);
+void criar_disciplina(Disciplina **ptr);
+void inserir_aluno_na_disciplina();
+void inserir_disciplina_no_aluno();
+void remover_matricula(Matricula **ptr);
+void remover_aluno(Aluno **ptr);
+void remover_disciplina(Disciplina **ptr);
+void listar_alunos_por_periodo(); //dispensável
+void listar_disciplinas_por_periodo(); //dispensável
+void consultar_disciplinas_por_aluno(Matricula **ptr);
+void consultar_alunos_por_disciplina(Matricula **ptr);
+//  Perceba que o return da função procurar é um ponteiro com o endereço da 
+// struct, visto que vamos usar essa função pra modificar, apagar ou printar os 
+// dados da matricula, do aluno ou da discilina;
+Aluno *procurar_aluno(Aluno *ptr);
+Disciplina *procurar_disciplina(Disciplina *ptr);
+Matricula *procurar_matricula(Matricula *ptr);
+void liberar();
+void salvar();
+void recuperar();
+/******************************************************************************
+ * nesse começo de código, vamos considerar que serão feitas apenas cadastros *
+ * válidos, porém é interessante colocar algumas funções de validação,        *
+ * principalmente para os códigos e para o período                            *  
+ ******************************************************************************/
+
+
+//declaração de variáveis globais
+
+
+int main (){
+  //primeiro passoda main: recurar os dados salvos
+  recuperar();
+  p("\t\tSISTEMA DE MATRÍCULAS\t\t\n");
+  //variáveis para controle da opção escolhida em cada menu
+  int op1, op2;
+  do{
+    op1 = -1;
+    p("escolha uma opção:\n");
+    p("1- Criar\n");
+    p("2- Inserção\n");
+    p("3- Remoção\n");
+    p("4- Consulta\n");
+    p("0- Salvar e sair\n");
+    s(" %d", &op1);
+    /**************************************************************************
+     * getchar é usado para pegar o char do buffer, no caso do usuário digitar*
+     * uma letra, sem ele o menu entraria num loop infinito                   *
+     **************************************************************************/
+    getchar();
+
+    if(op1 < 0 || op1 > 3){
+      p("op1 recebeu um valor inválido\n");
+    }
+
+    switch(op1){
+      case 1:
+        do{
+          op2 = -1;
+          p("escolha uma opção:\n");
+          p("1- Criar nova ficha de aluno\n");
+          p("2- Criar nova ficha de disciplina\n");
+          p("0- voltar para o menu principal\n");
+          s(" %d", &op2);
+          getchar();
+
+          switch(op2){
+            case 0:
+              break;
+            case 1:
+              criar_aluno(&ptr_i_aluno);
+              break;
+            case 2:
+              criar_disciplina(&ptr_i_disciplina);
+              break;
+            default:
+              p("Opção inválida\n");
+              break;
+          }
+        }while(op2);
+        break;
+      case 2:
+        do{
+          op2 = -1;
+          p("escolha uma opção:\n");
+          p("1- Inserir novos alunos para disciplina\n");
+          p("2- Inserir novas disciplinas para aluno\n");
+          p("0- voltar para o menu principal\n");
+          s(" %d", &op2);
+          getchar();
+
+
+          switch(op2){
+            case 1:
+              inserir_aluno_na_disciplina();
+              break;
+            case 2:
+              inserir_disciplina_no_aluno();
+              break;
+            default:
+              p("Opção inválida\n");
+              break;
+          }
+        }while(op2);
+        op1 = -1;
+        break;
+      case 3:
+        do{
+          p("escolha uma opção:\n");
+          p("1- Remover matricula de aluno\n");
+          p("2- Remover Ficha de aluno\n");
+          p("3- Remover Fciha de disciplina\n");
+          p("0- voltar para o menu principal\n");
+          s(" %d", &op2);
+          getchar(); 
+
+          switch(op2){
+            case 1:
+              remover_matricula(&ptr_i_matricula);
+              break;
+            case 2:
+              remover_aluno(&ptr_i_aluno);
+              break;
+            case 3:
+              remover_disciplina(&ptr_i_disciplina);
+              break;
+            default:
+              p("Opção inválida\n");
+              break;
+          }
+        }while(op2);
+        break; 
+      case 4:
+        do{
+          p("escolha uma opção:\n");
+          p("1- Consultar disciplinas de um aluno\n");
+          p("2- Consultar alunos matriculados em uma disciplina\n");
+          p("0- voltar para o menu principal\n");
+          s(" %d", &op2);
+          getchar(); 
+
+          switch(op2){
+            case 1:
+              consultar_disciplinas_por_aluno(&ptr_i_matricula);
+              break;
+            case 2:
+              consultar_alunos_por_disciplina(&ptr_i_matricula);
+              break;
+            default:
+              p("Opção inválida\n");
+              break;
+          }
+        }while(op2);
+        break; 
+      
+    }
+    
+  }while(op1);
+
+  //antes de finalizar a main: salvar os dados e liberar as alocações
+  salvar();
+  liberar(ptr_i_aluno, ptr_i_disciplina, ptr_i_matricula);
+  return 0;
+}
+
+//lógica das funções
+/******************************************************************************
+ * Perceba que no início de cada função tem um print com o nome da função     *
+ * após a criação da lógica da main e antes da criação da lógica das funções  *
+ * rodo o programa uma vez para testes, e para isso, preciso que as funções   *
+ * tenham algum retorno para eu saber se elas estão sendo chamadas            *
+ ******************************************************************************/
+
+void criar_matricula(Matricula **ptr){
+  p("chamada a função criar_matricula\n");
+  // ponteiro de controle para ordenar a lista encadeada
+  // as variaveis serao ordenadas no momento da criacao
+  // significado: new_ptr e old_ptr;
+  Matricula *n_ptr, *o_ptr, *ptr_aux;
+
+  n_ptr = (Matricula*)malloc(sizeof(Matricula));
+  if(!n_ptr){
+    p("Erro na alocação!\n");
+    return;
+  }
+
+  //coleta dos dados
+  p("digite o período:\n");
+  s(" %f", &(n_ptr->periodo));
+
+  p("digite o código da aluno:\n");
+  s(" %d", &(n_ptr->aluno));
+
+  p("digite o código do disciplina:\n");
+  s(" %d", &(n_ptr->disciplina));
+
+  //inserção na lista encadeada
+  ptr_aux = *ptr;
+  o_ptr = NULL;
+
+  //caso seja o primeiro cadastro
+  if(!ptr_aux){
+    n_ptr->prox = NULL;
+    n_ptr->ant = NULL;
+    *ptr = n_ptr;
+  } else {
+    //mesmo principio do insertion sort
+    //irei ordenar por periodo, codigo do aluno, codigo da disciplina
+    while(
+      ptr_aux && 
+      ptr_aux->periodo < n_ptr->periodo &&
+      ptr_aux->aluno < n_ptr->aluno &&
+      ptr_aux->disciplina < n_ptr->disciplina 
+    ){
+      o_ptr = ptr_aux;
+      ptr_aux = ptr_aux->prox;
+    }
+
+    n_ptr->prox = ptr_aux;
+    ptr_aux->ant = n_ptr;
+
+    //caso em que o elemento será colocado como o primeiro da lista
+    if(!o_ptr){
+      *ptr = n_ptr;
+      n_ptr->ant = NULL;
+    } else{
+      o_ptr->prox = n_ptr;
+      n_ptr->ant = o_ptr;
+    }
+  }
+  return;
+}
+
+void criar_aluno(Aluno **ptr){
+  p("chamada a função criar_aluno\n");
+  Aluno *n_ptr, *o_ptr, *ptr_aux;
+
+  n_ptr = (Aluno*)malloc(sizeof(Aluno));
+  if(!n_ptr){
+    p("Erro na alocação!\n");
+    return;
+  }
+
+  char string_var[100];
+  p("digite o nome do aluno:\n");
+  s(" %[^\n]", string_var);
+  strcpy(n_ptr->nome, string_var);
+
+  p("digite o CPF do aluno no formato XXXXXXXXX-XX:\n");
+  s(" %[^\n]", string_var);
+  strcpy(n_ptr->cpf, string_var);
+
+  p("digite o código do aluno:\n");
+  s(" %f", &(n_ptr->codigo));
+
+  ptr_aux = *ptr;
+  o_ptr = NULL;
+
+  if(!ptr_aux){
+    n_ptr->prox = NULL;
+    n_ptr->ant = NULL;
+    *ptr = n_ptr;
+  } else {
+    while(
+      ptr_aux && 
+      ptr_aux->codigo < n_ptr->codigo
+    ){
+      o_ptr = ptr_aux;
+      ptr_aux = ptr_aux->prox;
+    }
+
+    n_ptr->prox = ptr_aux;
+    ptr_aux->ant = n_ptr;
+
+    if(!o_ptr){
+      *ptr = n_ptr;
+      n_ptr->ant = NULL;
+    } else{
+      o_ptr->prox = n_ptr;
+      n_ptr->ant = o_ptr;
+    }
+  }
+  return;
+}
+
+void criar_disciplina(Disciplina **ptr){
+  p("chamada a função criar_disciplina\n");
+  Disciplina *n_ptr, *o_ptr, *ptr_aux;
+
+  n_ptr = (Disciplina*)malloc(sizeof(Disciplina));
+  if(!n_ptr){
+    p("Erro na alocação!\n");
+    return;
+  }
+
+  char string_var[100];
+  p("digite o nome da disciplina:\n");
+  s(" %[^\n]", string_var);
+  strcpy(n_ptr->nome, string_var);
+
+  p("digite o nomde do professor:\n");
+  s(" %[^\n]", string_var);
+  strcpy(n_ptr->professor, string_var);
+
+  p("digite os créditos da disciplina:\n");
+  s(" %f", &(n_ptr->creditos));
+
+  p("digite o código da disciplina:\n");
+  s(" %f", &(n_ptr->codigo));
+
+  ptr_aux = *ptr;
+  o_ptr = NULL;
+
+  if(!ptr_aux){
+    n_ptr->prox = NULL;
+    n_ptr->ant = NULL;
+    *ptr = n_ptr;
+  } else {
+    while(
+      ptr_aux && 
+      ptr_aux->codigo < n_ptr->codigo
+    ){
+      o_ptr = ptr_aux;
+      ptr_aux = ptr_aux->prox;
+    }
+
+    n_ptr->prox = ptr_aux;
+    ptr_aux->ant = n_ptr;
+
+    if(!o_ptr){
+      *ptr = n_ptr;
+      n_ptr->ant = NULL;
+    } else{
+      o_ptr->prox = n_ptr;
+      n_ptr->ant = o_ptr;
+    }
+  }
+  return;
+}
+
+
+void inserir_aluno_na_disciplina(){
+  p("chamada a função inserir_aluno_na_disciplina\n");
+  criar_matricula(&ptr_i_matricula);
+}
+void inserir_disciplina_no_aluno(){
+  p("chamada a função inserir_disciplina_no_aluno\n");
+  criar_matricula(&ptr_i_matricula);
+}
+
+void remover_matricula(Matricula **ptr){
+  p("chamada a função remover_matricula\n");
+  Matricula *o_ptr, *ptr_aux;
+  float per_aux;
+  int al_aux, dis_aux;
+
+  o_ptr = NULL;
+  ptr_aux = *ptr;
+
+  p("digite o periodo em que deseja cancelar a matricula\n");
+  s(" %f", &per_aux);
+
+  p("digite o código do aluno deseja cancelar a matricula\n");
+  s(" %d", &al_aux);
+
+  p("digite o código da disciplina que deseja cancelar a matricula\n");
+  s(" %d", &dis_aux);
+
+  while(
+    ptr_aux && 
+    ptr_aux->periodo < per_aux &&
+    ptr_aux->aluno < al_aux &&
+    ptr_aux->disciplina < dis_aux  
+  ){
+    o_ptr = ptr_aux;
+    ptr_aux = ptr_aux->prox;
+  }
+
+  if(
+    ptr_aux->periodo == per_aux &&
+    ptr_aux->aluno == al_aux &&
+    ptr_aux->disciplina == dis_aux
+  ){
+    o_ptr = ptr_aux->prox;
+    ptr_aux->prox->ant = o_ptr;
+    free(ptr_aux);
+    p("Matricula removida com sucesso\n");
+  }else{
+    p("Matricula não encontrada\n");
+  }
+
+  return;
+}
+
+void remover_aluno(Aluno **ptr){
+  p("chamada a função remover_aluno\n");
+  Aluno *o_ptr, *ptr_aux;
+  int al_aux;
+
+  o_ptr = NULL;
+  ptr_aux = *ptr;
+
+  p("Digite o código do aluno deseja remover\n");
+  s(" %d", &al_aux);
+
+  while(
+    ptr_aux &&
+    ptr_aux->codigo < al_aux
+  ){
+    o_ptr = ptr_aux;
+    ptr_aux = ptr_aux->prox;
+  }
+
+  if(
+    ptr_aux->codigo == al_aux
+  ){
+    o_ptr = ptr_aux->prox;
+    ptr_aux->prox->ant = o_ptr;
+    free(ptr_aux);
+    p("Aluno removido com sucesso\n");
+  }else{
+    p("Aluno não encontrado\n");
+  }
+
+  return;
+}
+
+void remover_disciplina(Disciplina **ptr){
+  p("chamada a função remover_disciplina\n");
+  Disciplina *o_ptr, *ptr_aux;
+  int dis_aux;
+
+  o_ptr = NULL;
+  ptr_aux = *ptr;
+
+  p("digite o código da disciplina que deseja cancelar a matricula\n");
+  s(" %d", &dis_aux);
+
+  while(
+    ptr_aux &&
+    ptr_aux->codigo < dis_aux  
+  ){
+    o_ptr = ptr_aux;
+    ptr_aux = ptr_aux->prox;
+  }
+
+  if(
+    ptr_aux->codigo == dis_aux
+  ){
+    o_ptr = ptr_aux->prox;
+    ptr_aux->prox->ant = o_ptr;
+    free(ptr_aux);
+    p("Disciplina removida com sucesso\n");
+  }else{
+    p("Disciplina não encontrada\n");
+  }
+
+  return;
+}
+
+void consultar_disciplinas_por_aluno(Matricula **ptr){
+  p("chamada a função consultar_disciplinas_por_aluno\n");
+  Matricula *ptr_aux;
+  float per_aux;
+  int al_aux;
+
+  p("Digite o período de matricula do aluno\n");
+  s(" %f", &per_aux);
+
+  p("Digite o código do aluno que deseja fazer a consulta\n");
+  s(" %d", &al_aux);
+
+  while(ptr_aux && ptr_aux->periodo < per_aux){
+    ptr_aux = ptr_aux->prox;
+  }
+  while(ptr_aux && ptr_aux->periodo == per_aux && ptr_aux->aluno < al_aux){
+    ptr_aux = ptr_aux->prox;
+  }
+  if(ptr_aux && ptr_aux->periodo == per_aux && ptr_aux->aluno == al_aux){
+    p("Lista de disciplinas em que o aluno %d está matriculado no período %f\n", al_aux, per_aux);
+    int count = 0;
+    while(ptr_aux && ptr_aux->periodo == per_aux && ptr_aux->aluno == al_aux){
+      p("%d-\t%d\n", count++, ptr_aux->disciplina);
+    }
+  }else{
+    p("O aluno não está matriculado em nenhuma disciplina no período fornecido\n");
+  }
+  return;
+}
+
+void consultar_alunos_por_disciplina(Matricula **ptr){
+  p("chamada a função consultar_alunos_por_disciplinas\n");
+  Matricula *ptr_aux;
+  float per_aux;
+  int dis_aux;
+
+  p("Digite o período de matricula do aluno\n");
+  s(" %f", &per_aux);
+
+  p("Digite o código da disciplina que deseja fazer a consulta\n");
+  s(" %d", &dis_aux);
+
+  while(ptr_aux && ptr_aux->periodo < per_aux){
+    ptr_aux = ptr_aux->prox;
+  }
+  int count = 0;
+  if(ptr_aux && ptr_aux->periodo == per_aux){
+    while(ptr_aux && ptr_aux->periodo == per_aux){
+      if(ptr_aux->disciplina == dis_aux){
+        if(!count){
+          p("Lista de alunos que estão matriculados na disicplina %d no período %f\n", dis_aux, per_aux);
+        }
+        p("%d-\t%d\n", count++, ptr_aux->aluno);
+      }else{
+        ptr_aux = ptr_aux->prox;
+      }
+    }
+  }
+  if(!count){
+    p("A disicplina nõa possui alunos cadastranso no período selecionado\n");
+  }
+  return;
+}
+
+Aluno *procurar_aluno(Aluno *ptr){
+  p("chamada a função procurar_aluno\n");
+  return ptr;
+}
+
+Disciplina *procurar_disciplina(Disciplina *ptr){
+  p("chamada a função procurar_disciplina\n");
+  return ptr;
+}
+
+Matricula *procurar_matricula(Matricula *ptr){
+  p("chamada a função procurar_matricula\n");
+  return ptr;
+}
+
+void liberar(Aluno *ptr_aluno, Disciplina *ptr_disciplina, Matricula *ptr_matricula){
+  p("chamada a função liberar\n");
+    if(ptr_aluno != NULL){
+    while(ptr_aluno->prox != NULL){
+      if(ptr_aluno->ant != NULL) free(ptr_aluno->ant);
+      ptr_aluno = ptr_aluno->prox;
+    }
+    if(ptr_aluno->ant != NULL) free(ptr_aluno->ant);
+    free(ptr_aluno);
+  }
+
+  if(ptr_disciplina != NULL){
+    while(ptr_disciplina->prox != NULL){
+      if(ptr_disciplina->ant != NULL) free(ptr_disciplina->ant);
+      ptr_disciplina = ptr_disciplina->prox;
+    }
+    if(ptr_disciplina->ant != NULL) free(ptr_disciplina->ant);
+    free(ptr_disciplina);
+  }
+
+  if(ptr_matricula != NULL){
+    while(ptr_matricula->prox != NULL){
+      if(ptr_matricula->ant != NULL) free(ptr_matricula->ant);
+      ptr_matricula = ptr_matricula->prox;
+    }
+    if(ptr_matricula->ant != NULL) free(ptr_matricula->ant);
+    free(ptr_matricula);
+  }
+}
+
+void salvar(){
+  p("chamada a função salvar\n");
+}
+
+void recuperar(){
+  p("chamada a função recuperar\n");
+}
